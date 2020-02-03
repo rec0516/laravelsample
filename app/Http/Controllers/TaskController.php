c10f146c0daa9ec9106ed0dce4394723a1afd4d2
rec0516 <akira.coffee@gmail.com>
2020-02-04 00:25:31 +0900

    タスクの編集機能作成の途中

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -34,4 +34,14 @@ class TaskController extends Controller
         ]);
 
     }
+
+    public function showEditForm(int $id, int $task_id)
+    {
+        $task = Task::find($task_id);
+
+        return view('tasks/edit', [
+            'task' => $task,
+        ]);
+
+    }
 }
